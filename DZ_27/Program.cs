d100807100e7f54a6c5fbecda72bd6bcb2384be3
Grisha_Pinigin <pin.pinigin@mail.ru>
2022-09-01 11:51:46 +0500

    Задача 27

--- DZ_27/Program.cs
+++ DZ_27/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int SumNumber(int number)
+{
+    
+    int Length = Convert.ToString(number).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < Length; i++)
+    {
+        advance = number - number % 10; 
+        result = result + (number - advance);
+        number = number / 10;
+    }
+    return result;
+}
+
+Console.WriteLine(SumNumber(num));
+
+
+// int num = 4 - 4 % 10; //450 0
+// int num2 = 7 + (4 - 0); //2 7
+// int num3 = 45/10;//45 4
+// Console.WriteLine(num3);
+

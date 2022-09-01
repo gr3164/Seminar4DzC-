150e51c17bfd20cd44498f4211c048e23e8a579a
Grisha_Pinigin <pin.pinigin@mail.ru>
2022-09-01 09:58:28 +0500

    Задача 25(Второй способ)

--- DZ_25/Program.cs
+++ DZ_25/Program.cs
@@ -10,11 +10,28 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
+//первый способ
+
+// int StepNumber(int num1, int num2)
+// {
+
+//    double pow = Math.Pow (num1,num2);
+//    return Convert.ToInt32(pow);
+// }
+
+//Второй способ
+
 int StepNumber(int num1, int num2)
 {
-
-   double pow = Math.Pow (num1,num2);
-   return Convert.ToInt32(pow);
+    int sum = 0;
+    int i = 0;
+    while (i < num2)
+    {
+        i++;
+        double pow = Math.Pow (num1,i);
+        sum = Convert.ToInt32(pow);   
+    }
+    return sum;
 }
 
 

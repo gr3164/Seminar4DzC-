ea9caabd6ee78702de7372af81c4a3ec66f4a7c9
Grisha_Pinigin <pin.pinigin@mail.ru>
2022-09-01 10:41:35 +0500

    Задача 29

---
+++ DZ_29/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] arr = new int[8];
+
+void FillArray(int[] Array)
+{
+    int length = Array.Length;
+    int index = 0;
+    while(index < length)
+    {
+        Array[index] = new Random().Next(0, 100);
+        Console.Write(arr[index] + ", ");
+        index++;
+    }
+}
+
+
+FillArray(arr);
+Console.WriteLine();
